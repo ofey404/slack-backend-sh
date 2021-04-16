ba4b919fcca48c2e234afc6025c1c0ad28f23569
Ofey Chan <ofey206@gmail.com>
2021-04-17 04:47:30 +0800

    feat: make users.list work

--- main.sh
+++ main.sh
@@ -14,7 +14,10 @@ SAVE_PATH=$(init_parameter SAVE_PATH ./data)
 mkdir -p $SAVE_PATH
 
 LIST_FILE_PATH="$SAVE_PATH/list.json"
+USERS_FILE_PATH="$SAVE_PATH/users.json"
 
-slack_conversation_list_wrapper $API_TOKEN | jq > $LIST_FILE_PATH
+slack_conversations_list_wrapper $API_TOKEN | jq > $LIST_FILE_PATH
+slack_users_list_wrapper $API_TOKEN | jq > $USERS_FILE_PATH
 
 all_channel_id_from_list_file $LIST_FILE_PATH
+extract_users_info_from_users_file $USERS_FILE_PATH
--- slack.sh
+++ slack.sh
@@ -31,7 +31,7 @@ function all_channel_id_from_list_file() {
     return 0
 }
 
-function slack_conversation_list_wrapper() {
+function slack_conversations_list_wrapper() {
     local api_token="$1"
     local optional_arguments="${@:2}"
 
@@ -62,4 +62,39 @@ function slack_conversation_list_wrapper() {
 
 # users.list
 # https://api.slack.com/methods/users.list
-# TODO
+
+function slack_users_list_wrapper() {
+    local api_token="$1"
+    local optional_arguments="${@:2}"
+
+    local data=$(join_arguments_www_form "token=$API_TOKEN" "$optional_arguments")
+
+    while :
+    do
+        resp=$(curl -X GET -H "Authorization: Bearer $api_token" \
+             -H "Content-type: application/x-www-form-urlencoded" \
+             --data "$data"\
+             https://slack.com/api/users.list)
+
+        echo "$resp"
+        # If no next cursor, break
+        if [[ "\"\"" == `echo "$resp" | jq ".response_metadata.next_cursor"` ]]; then
+            break
+        fi
+
+        # TODO
+        echo "TODO: handle cursor" 1>&2
+        exit 1
+    done
+
+    echo "$resp"
+    return 0
+}
+
+# Extract user info needed by jq. Just a demo.
+# Change patterns inside if more information is wanted.
+function extract_users_info_from_users_file() {
+    local users_file_path="$1"
+    cat $users_file_path | jq '.members[] | {id: .id, display_name: .profile.display_name}'
+}
+
